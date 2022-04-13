4296c0a49a5914a49dfe26d4c1e2d71d556dcc10
mashuqian <mashuqian@meican.com>
2022-04-13 21:36:44 +0800

    第二次提交

--- app/src/androidTest/java/zone/bokmark/espressokit/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/zone/bokmark/espressokit/ExampleInstrumentedTest.kt
@@ -1,24 +1,52 @@
 package zone.bokmark.espressokit
 
-import androidx.test.platform.app.InstrumentationRegistry
+import android.content.Context
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.filters.LargeTest
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.uiautomator.UiDevice
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+
 @RunWith(AndroidJUnit4::class)
+@LargeTest
 class ExampleInstrumentedTest {
+
+    lateinit var mDevice: UiDevice
+
+    lateinit var appContext: Context
+
+    @get:Rule
+    var activityScenarioRule = activityScenarioRule<MainActivity>()
+
+    @Before
+    fun init() {
+        appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+    }
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("zone.bokmark.espressokit", appContext.packageName)
+    }
+
+
+    @Test
+    fun testTextView() {
+        Espresso.onView(ViewMatchers.withId(R.id.tv)).checkText("Hello World!")
     }
 }
